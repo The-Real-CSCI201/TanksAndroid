d09e66ba71e58cbdeaf34473ef9977293a65b672
Vinnie Magro <vmagro@socaldevs.com>
2014-11-24 21:03:09 -0800

    Log body

--- app/src/main/java/edu/usc/csci201/tanks/MainActivity.java
+++ app/src/main/java/edu/usc/csci201/tanks/MainActivity.java
@@ -266,6 +266,7 @@ public class MainActivity extends Activity implements GameListFragment.GameListF
                 Toast.makeText(MainActivity.this, "Failed to join game", Toast.LENGTH_LONG).show();
                 error.printStackTrace();
                 Log.e(TAG, "failed url = " + error.getUrl());
+                Log.e(TAG, "body = " + error.getBody().toString());
             }
         });
     }
