92dd34a3bcd08977a449f20987085e9e02f46309
Carrie Sun <carriesu@usc.edu>
2014-11-24 20:52:53 -0800

    changed to use orientation enum

--- app/src/main/java/edu/usc/csci201/tanks/gameplay/Wall.java
+++ app/src/main/java/edu/usc/csci201/tanks/gameplay/Wall.java
@@ -1,12 +1,14 @@
 package edu.usc.csci201.tanks.gameplay;
 
+import edu.usc.csci201.tanks.common.Orientation;
+
 /**
  * Created by carrie on 11/24/14.
  */
 public class Wall {
     private int row, col;
-    private int orientation;
-    public Wall(int row, int col, int orientation)
+    private Orientation orientation;
+    public Wall(int row, int col, Orientation orientation)
     {
         this.row = row;
         this.col = col;
@@ -20,7 +22,7 @@ public class Wall {
     {
         return col;
     }
-    public int getOrientation()
+    public Orientation getOrientation()
     {
         //return something based on the enum
         return orientation;//subject to change?
