e21ea7d017dd3dc869a1aa0c0c5e871683aa61da
Vinnie Magro <vmagro@socaldevs.com>
2014-11-24 20:53:54 -0800

    Fix Nick's problem

--- app/src/main/java/edu/usc/csci201/tanks/MainActivity.java
+++ app/src/main/java/edu/usc/csci201/tanks/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends Activity implements GameListFragment.GameListF
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
                     .add(R.id.container, new GameListFragment())
