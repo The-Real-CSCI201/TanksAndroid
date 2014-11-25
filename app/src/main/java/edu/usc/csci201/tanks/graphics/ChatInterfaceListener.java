e3db7ffd3f2f1b391b0a35c48d5d13efe5b980dd
Nick Entin <nickentin@nickentin.com>
2014-11-24 21:56:28 -0800

    Added image getter to chat listener

--- app/src/main/java/edu/usc/csci201/tanks/graphics/ChatInterfaceListener.java
+++ app/src/main/java/edu/usc/csci201/tanks/graphics/ChatInterfaceListener.java
@@ -1,11 +1,15 @@
 package edu.usc.csci201.tanks.graphics;
 
+import android.graphics.Bitmap;
+
 /**
  * (C) 2014 nickentin
  * Created on 11/19/14.
  */
 public interface ChatInterfaceListener {
     public void userDidSelectChannel(ChatChannel channel);
+
+    public Bitmap getImageForUser(String userid);
 
     public enum ChatChannel { ALL, TEAM, USER1, USER2, USER3 }
 }
