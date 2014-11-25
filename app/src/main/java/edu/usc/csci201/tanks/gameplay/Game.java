504a9d1327b9b6da9c48e4ac82c79869add38f06
Vinnie Magro <vmagro@socaldevs.com>
2014-11-24 20:24:57 -0800

    Fix broken import

--- app/src/main/java/edu/usc/csci201/tanks/gameplay/Game.java
+++ app/src/main/java/edu/usc/csci201/tanks/gameplay/Game.java
@@ -1,13 +1,15 @@
 package edu.usc.csci201.tanks.gameplay;
 
+import java.util.List;
+
 /**
  * Created by carrieksun on 11/23/2014.
  */
 public class Game {
     private List<Player> players;
     private GameMap map;
-    public Game()
-    {
+
+    public Game() {
         //how will the setting of players and map work? in the constructor or not?
     }
 }
