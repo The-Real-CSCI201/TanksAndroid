df072e2868cc283ac77787036e88f2adac1d73da
Vinnie Magro <vmagro@socaldevs.com>
2014-11-27 00:11:55 -0800

    Fix compilation error

--- app/src/main/java/edu/usc/csci201/tanks/graphics/DebugChatListener.java
+++ app/src/main/java/edu/usc/csci201/tanks/graphics/DebugChatListener.java
@@ -1,5 +1,7 @@
 package edu.usc.csci201.tanks.graphics;
 
+import android.graphics.Bitmap;
+
 /**
  * (C) 2014 nickentin
  * Created on 11/24/14.
@@ -8,5 +10,10 @@ public class DebugChatListener implements ChatInterfaceListener {
     @Override
     public void userDidSelectChannel(ChatChannel channel) {
 
+    }
+
+    @Override
+    public Bitmap getImageForUser(String userid) {
+        return null;
     }
 }
