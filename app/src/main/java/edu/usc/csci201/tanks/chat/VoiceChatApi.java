2280b55b91b696b98f1f20bc4b193507290b4308
Vinnie Magro <vmagro@socaldevs.com>
2014-11-30 13:01:55 -0800

    Merge in chat api

---
+++ app/src/main/java/edu/usc/csci201/tanks/chat/VoiceChatApi.java
@@ -0,0 +1,23 @@
+package edu.usc.csci201.tanks.chat;
+
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.http.POST;
+
+/**
+ * Created by vmagro on 11/27/14.
+ */
+public interface VoiceChatApi {
+
+    public static VoiceChatApi VoiceChatApi = new RestAdapter.Builder()
+            .setEndpoint("http://voicechatapi.com/api/v1")
+            .setLogLevel(RestAdapter.LogLevel.FULL)
+            .build().create(VoiceChatApi.class);
+
+    @POST("/conference/")
+    public void createConference(Callback<Conference> callback);
+
+    @POST("/conference/")
+    public Conference createConference();
+
+}
